496ac404771ffbfd9c605f0e41d6849409aee8b7
yasupeter <peterleeful@gmail.com>
2023-11-10 12:03:13 +0900

    Fri, 11.10 회원가입 폼 작성

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,12 +2,13 @@ import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 export function NavBar() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <Flex>
-            <Button onClick={() => navigate("/")}>home</Button>
-            <Button onClick={() => navigate("/write")}>write</Button>
-        </Flex>
-    );
+  return (
+    <Flex>
+      <Button onClick={() => navigate("/")}>home</Button>
+      <Button onClick={() => navigate("/write")}>write</Button>
+      <Button onClick={() => navigate("/signup")}>signup</Button>
+    </Flex>
+  );
 }
