b763953adf91fb8436f93fe9aaa13787e2edf7b9
yasupeter <peterleeful@gmail.com>
2023-11-10 11:26:34 +0900

    Fri, 11.10 수정 버튼 클릭시 수정화면(/edit/:id)으로 이동

---
+++ src/page/BoardEdit.js
@@ -0,0 +1,88 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useImmer } from "use-immer";
+import axios from "axios";
+import { logDOM } from "@testing-library/react";
+
+export function BoardEdit() {
+  const [board, updateBoard] = useImmer(null);
+
+  // /edit/:id
+  const { id } = useParams();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get("/api/board/id/" + id)
+      .then((response) => updateBoard(response.data));
+  }, []);
+
+  if (board === null) {
+    return <Spinner />;
+  }
+
+  function handleSubmit() {
+    // 저장 버튼 클릭 시
+    // PUT /api/board/edit
+
+    axios
+      .put("/api/board/edit", board)
+      .then(() => console.log("잘됨"))
+      .catch(() => console.log("잘안됨"))
+      .finally(() => console.log("끝"));
+  }
+
+  return (
+    <Box>
+      <h1>{id}번 글 수정</h1>
+      <FormControl>
+        <FormLabel>제목</FormLabel>
+        <Input
+          value={board.title}
+          onChange={(e) =>
+            updateBoard((draft) => {
+              draft.title = e.target.value;
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>본문</FormLabel>
+        <Textarea
+          value={board.content}
+          onChange={(e) =>
+            updateBoard((draft) => {
+              draft.content = e.target.value;
+            })
+          }
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel>작성자</FormLabel>
+        <Input
+          value={board.writer}
+          onChange={(e) =>
+            updateBoard((draft) => {
+              draft.writer = e.target.value;
+            })
+          }
+        />
+      </FormControl>
+      <Button colorScheme="blue" onClick={handleSubmit}>
+        저장
+      </Button>
+      {/* navigate(-1) : 이전 경로로 이동 */}
+      <Button onClick={() => navigate(-1)}>취소</Button>
+    </Box>
+  );
+}
