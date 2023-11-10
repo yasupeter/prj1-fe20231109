8f6b9580e474818f2faadc9a4cdff6d60e1283a0
yasupeter <peterleeful@gmail.com>
2023-11-10 11:11:28 +0900

    Fri, 11.10 게시물 보기 페이지 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
 } from "react-router-dom";
 import { BoardWrite } from "./page/BoardWrite";
 import { BoardList } from "./page/BoardList";
 import { HomeLayout } from "./layout/HomeLayout";
+import { BoardView } from "./page/BoardView";
 
 const routes = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<HomeLayout />}>
-          <Route index element={<BoardList />} />
-          <Route path="write" element={<BoardWrite />} />
+            <Route index element={<BoardList />} />
+            <Route path="write" element={<BoardWrite />} />
+            <Route path="board/:id" element={<BoardView />} />
         </Route>,
     ),
 );
 
 function App(props) {
-  return <RouterProvider router={routes} />;
+    return <RouterProvider router={routes} />;
 }
 
 export default App;
