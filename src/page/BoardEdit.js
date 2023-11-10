eebb8eb79a9a41cdd258ffe1ef2699361c5149be
yasupeter <peterleeful@gmail.com>
2023-11-10 11:27:18 +0900

    Fri, 11.10 제목과 콘텐트 변경 가능 handle... 함수 완성

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -1,88 +1,59 @@
 import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Spinner,
-  Textarea,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Spinner,
+    Textarea,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useImmer } from "use-immer";
 import axios from "axios";
-import { logDOM } from "@testing-library/react";
 
 export function BoardEdit() {
-  const [board, updateBoard] = useImmer(null);
+    const [board, updateBoard] = useImmer(null);
 
-  // /edit/:id
-  const { id } = useParams();
+    // /edit/:id
+    const { id } = useParams();
 
-  const navigate = useNavigate();
+    useEffect(() => {
+        axios
+            .get("/api/board/id/" + id)
+            .then((response) => updateBoard(response.data));
+    }, []);
 
-  useEffect(() => {
-    axios
-      .get("/api/board/id/" + id)
-      .then((response) => updateBoard(response.data));
-  }, []);
+    if (board === null) {
+        return <Spinner />;
+    }
 
-  if (board === null) {
-    return <Spinner />;
-  }
+    function handleTitleChange(e) {
+        updateBoard((draft) => {
+            draft.title = e.target.value;
+        });
+    }
 
-  function handleSubmit() {
-    // 저장 버튼 클릭 시
-    // PUT /api/board/edit
+    function handleContentChange(e) {
+        updateBoard((draft) => {
+            draft.content = e.target.value;
+        });
+    }
 
-    axios
-      .put("/api/board/edit", board)
-      .then(() => console.log("잘됨"))
-      .catch(() => console.log("잘안됨"))
-      .finally(() => console.log("끝"));
-  }
-
-  return (
-    <Box>
-      <h1>{id}번 글 수정</h1>
-      <FormControl>
-        <FormLabel>제목</FormLabel>
-        <Input
-          value={board.title}
-          onChange={(e) =>
-            updateBoard((draft) => {
-              draft.title = e.target.value;
-            })
-          }
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel>본문</FormLabel>
-        <Textarea
-          value={board.content}
-          onChange={(e) =>
-            updateBoard((draft) => {
-              draft.content = e.target.value;
-            })
-          }
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel>작성자</FormLabel>
-        <Input
-          value={board.writer}
-          onChange={(e) =>
-            updateBoard((draft) => {
-              draft.writer = e.target.value;
-            })
-          }
-        />
-      </FormControl>
-      <Button colorScheme="blue" onClick={handleSubmit}>
-        저장
-      </Button>
-      {/* navigate(-1) : 이전 경로로 이동 */}
-      <Button onClick={() => navigate(-1)}>취소</Button>
-    </Box>
-  );
+    return (
+        <Box>
+            <h1>{id}번 글 수정</h1>
+            <FormControl>
+                <FormLabel>제목</FormLabel>
+                <Input value={board.title} onChange={handleTitleChange} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>본문</FormLabel>
+                <Textarea value={board.content} onChange={handleContentChange} />
+            </FormControl>
+            <FormControl>
+                <FormLabel>작성자</FormLabel>
+                <Input value={board.writer} />
+            </FormControl>
+        </Box>
+    );
 }
