8c3abdebc6eaf7e6e97fed6dd2296b14fadf9884
yasupeter <peterleeful@gmail.com>
2023-11-10 16:36:09 +0900

    Fri, 11.10 회원목록 링크 네브바에 추가

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,13 +2,14 @@ import { Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
 export function NavBar() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  return (
-    <Flex>
-      <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
-      <Button onClick={() => navigate("/signup")}>signup</Button>
-    </Flex>
-  );
+    return (
+        <Flex>
+            <Button onClick={() => navigate("/")}>home</Button>
+            <Button onClick={() => navigate("/write")}>write</Button>
+            <Button onClick={() => navigate("/signup")}>signup</Button>
+            <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+        </Flex>
+    );
 }
