980dde63a02920b3162c155ff3729307125e22c8
yasupeter <peterleeful@gmail.com>
2023-11-13 09:56:03 +0900

    Mon, 11.13 회원정보보기 기능 추가 중 (1)

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,43 +1,43 @@
 import { useSearchParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import {
-  Box,
-  FormControl,
-  FormLabel,
-  Spinner,
-  Input,
-  Button,
+    Box,
+    FormControl,
+    FormLabel,
+    Spinner,
+    Input,
+    Button,
 } from "@chakra-ui/react";
 import axios from "axios";
 
 export function MemberView() {
-  const [member, setMember] = useState(null);
-  // /member?id=userid
-  const [params] = useSearchParams();
+    const [member, setMember] = useState(null);
+    // /member?id=userid
+    const [params] = useSearchParams();
 
-  useEffect(() => {
-    axios
-      .get("/api/member?" + params.toString())
-      .then((response) => setMember(response.data));
-  }, []);
+    useEffect(() => {
+        axios
+            .get("/api/member?" + params.toString())
+            .then((response) => setMember(response.data));
+    }, []);
 
-  if (member === null) {
-    return <Spinner />;
-  }
+    if (member === null) {
+        return <Spinner />;
+    }
 
-  return (
-    <Box>
-      <h1>{member.id}님 정보</h1>
-      <FormControl>
-        <FormLabel>password</FormLabel>
-        <Input type="text" value={member.password} readOnly />
-      </FormControl>
-      <FormControl>
-        <FormLabel>email</FormLabel>
-        <Input value={member.email} readOnly />
-      </FormControl>
-      <Button colorScheme="purple">수정</Button>
-      <Button colorScheme="red">삭제</Button>
-    </Box>
-  );
+    return (
+        <Box>
+            <h1>{member.id}님 정보</h1>
+            <FormControl>
+                <FormLabel>password</FormLabel>
+                <Input type="text" value={member.password} readOnly />
+            </FormControl>
+            <FormControl>
+                <FormLabel>email</FormLabel>
+                <Input value={member.email} readOnly />
+            </FormControl>
+            <Button colorScheme="purple">수정</Button>
+            <Button colorScheme="red">삭제</Button>
+        </Box>
+    );
 }
