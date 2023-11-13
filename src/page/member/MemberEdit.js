c7ae441943e58f8581c7cd3b25f0bbfc0a4b72f6
yasupeter <peterleeful@gmail.com>
2023-11-13 12:25:08 +0900

    Mon, 11.13 수정 버튼 활성화 조건 추가

--- src/page/member/MemberEdit.js
+++ src/page/member/MemberEdit.js
@@ -15,6 +15,8 @@ import {
 export function MemberEdit() {
     const [member, setMember] = useState(null);
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordCheck, setPasswordCheck] = useState("");
     const [emailAvailable, setEmailAvailable] = useState(false);
 
     const toast = useToast();
@@ -37,6 +39,18 @@ export function MemberEdit() {
     }
 
     let emailChecked = sameOriginEmail || emailAvailable;
+
+    // 암호가 없으면 기존 암호
+    // 암호를 작성하면 새 암호, 암호확인 체크
+    let passwordChecked = false;
+
+    if (passwordCheck === password) {
+        passwordChecked = true;
+    }
+
+    if (password.length === 0) {
+        passwordChecked = true;
+    }
 
     if (member === null) {
         return <Spinner />;
@@ -71,8 +85,23 @@ export function MemberEdit() {
             <h1>{id}님 정보 수정</h1>
             <FormControl>
                 <FormLabel>password</FormLabel>
-                <Input type="text" />
+                <Input
+                    type="text"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </FormControl>
+
+            {password.length > 0 && (
+                <FormControl>
+                    <FormLabel>password 확인</FormLabel>
+                    <Input
+                        type="text"
+                        value={passwordCheck}
+                        onChange={(e) => setPasswordCheck(e.target.value)}
+                    />
+                </FormControl>
+            )}
 
             {/*  email을 변경하면(작성시작) 중복확인 다시 하도록  */}
             {/*  기존 email과 같으면 중복확인 안해도됨 */}
@@ -92,6 +121,9 @@ export function MemberEdit() {
                     </Button>
                 </Flex>
             </FormControl>
+            <Button isDisabled={!emailChecked || !passwordChecked} colorScheme="blue">
+                수정
+            </Button>
         </Box>
     );
 }
