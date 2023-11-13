0d14dc215b9d9726696d3e2a7b120fcfdfa1081f
yasupeter <peterleeful@gmail.com>
2023-11-13 12:26:14 +0900

    Mon, 11.13 수정 버튼 클릭시 update요청 put /api/member/edit {id, password, email}

--- src/page/member/MemberEdit.js
+++ src/page/member/MemberEdit.js
@@ -80,6 +80,13 @@ export function MemberEdit() {
             });
     }
 
+    function handleSubmit() {
+        // put /api/member/edit
+        // {id, password, email}
+
+        axios.put("/api/member/edit", { id: member.id, password, email });
+    }
+
     return (
         <Box>
             <h1>{id}님 정보 수정</h1>
@@ -121,7 +128,11 @@ export function MemberEdit() {
                     </Button>
                 </Flex>
             </FormControl>
-            <Button isDisabled={!emailChecked || !passwordChecked} colorScheme="blue">
+            <Button
+                isDisabled={!emailChecked || !passwordChecked}
+                colorScheme="blue"
+                onClick={handleSubmit}
+            >
                 수정
             </Button>
         </Box>
