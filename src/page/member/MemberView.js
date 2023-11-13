10c0170c0d8e9485144368d81a3c9a1ab052a9d3
yasupeter <peterleeful@gmail.com>
2023-11-13 10:41:21 +0900

    Mon, 11.13 회원탈퇴 기능

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,5 +1,5 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import {
     Box,
     FormControl,
@@ -7,6 +7,15 @@ import {
     Spinner,
     Input,
     Button,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalCloseButton,
+    ModalBody,
+    ModalFooter,
+    useDisclosure,
+    useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -14,6 +23,11 @@ export function MemberView() {
     const [member, setMember] = useState(null);
     // /member?id=userid
     const [params] = useSearchParams();
+
+    const { isOpen, onClose, onOpen } = useDisclosure();
+
+    const navigate = useNavigate();
+    const toast = useToast();
 
     useEffect(() => {
         axios
@@ -23,6 +37,40 @@ export function MemberView() {
 
     if (member === null) {
         return <Spinner />;
+    }
+
+    function handleDelete() {
+        // axios
+        // delete /api/member?id=userid
+        // ok -> home 이동, toast 띄우기
+        // error -> toast 띄우기
+        // finally -> modal 닫기
+
+        axios
+            .delete("/api/member?" + params.toString())
+            .then(() => {
+                toast({
+                    description: "회원 탈퇴하였습니다.",
+                    status: "success",
+                });
+                navigate("/");
+
+                // TODO : 로그아웃 기능 추가하기
+            })
+            .catch((error) => {
+                if (error.response.status === 401 || error.response.status === 403) {
+                    toast({
+                        description: "권한이 없습니다.",
+                        status: "error",
+                    });
+                } else {
+                    toast({
+                        description: "탈퇴 처리 중에 문제가 발생하였습니다.",
+                        status: "error",
+                    });
+                }
+            })
+            .finally(() => onClose());
     }
 
     return (
@@ -37,7 +85,26 @@ export function MemberView() {
                 <Input value={member.email} readOnly />
             </FormControl>
             <Button colorScheme="purple">수정</Button>
-            <Button colorScheme="red">삭제</Button>
+            <Button colorScheme="red" onClick={onOpen}>
+                탈퇴
+            </Button>
+
+            {/* 탈퇴 모달 */}
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>탈퇴 확인</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>탈퇴 하시겠습니까?</ModalBody>
+
+                    <ModalFooter>
+                        <Button onClick={onClose}>닫기</Button>
+                        <Button onClick={handleDelete} colorScheme="red">
+                            탈퇴
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </Box>
     );
 }
