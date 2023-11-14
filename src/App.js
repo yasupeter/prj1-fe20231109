f957537d724cb61686c253ab72c292061b3a2015
yasupeter <peterleeful@gmail.com>
2023-11-14 15:29:56 +0900

    Tues, 11.14 글 작성자 인지에 따라서 수정 버튼 출력 결정

--- src/App.js
+++ src/App.js
@@ -52,8 +52,14 @@ function App(props) {
         return login !== "";
     }
 
+    function hasAccess(userId) {
+        return login.id === userId;
+    }
+
     return (
-        <LoginContext.Provider value={{ login, fetchLogin, isAuthenticated }}>
+        <LoginContext.Provider
+            value={{ login, fetchLogin, isAuthenticated, hasAccess }}
+        >
             <RouterProvider router={routes} />
         </LoginContext.Provider>
     );
