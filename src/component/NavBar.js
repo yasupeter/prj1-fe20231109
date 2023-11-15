7e8bd83fa82a1b4277494cde4dfd9e54b3a3569f
yasupeter <peterleeful@gmail.com>
2023-11-15 09:23:24 +0900

    Tues, 11.14 로그인 후 자기 정보보기

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -5,41 +5,52 @@ import { useContext } from "react";
 import { LoginContext } from "./LogInProvider";
 
 export function NavBar() {
-    const { fetchLogin, login, isAuthenticated, isAdmin } =
-        useContext(LoginContext);
-    const toast = useToast();
+  const { fetchLogin, login, isAuthenticated, isAdmin } =
+    useContext(LoginContext);
+  const toast = useToast();
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleLogout() {
-        axios
-            .post("/api/member/logout")
-            .then(() => {
-                toast({
-                    description: "로그아웃 되었습니다.",
-                    status: "info",
-                });
-                navigate("/");
-            })
-            .finally(() => fetchLogin());
-    }
+  const urlParams = new URLSearchParams();
 
-    return (
-        <Flex>
-            <Button onClick={() => navigate("/")}>home</Button>
-            {isAuthenticated() && (
-                <Button onClick={() => navigate("/write")}>write</Button>
-            )}
-            {isAuthenticated() || (
-                <Button onClick={() => navigate("/signup")}>signup</Button>
-            )}
-            {isAdmin() && (
-                <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-            )}
-            {isAuthenticated() || (
-                <Button onClick={() => navigate("/login")}>로그인</Button>
-            )}
-            {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
-        </Flex>
-    );
+  if (login !== "") {
+    urlParams.set("id", login.id);
+  }
+
+  function handleLogout() {
+    axios
+      .post("/api/member/logout")
+      .then(() => {
+        toast({
+          description: "로그아웃 되었습니다.",
+          status: "info",
+        });
+        navigate("/");
+      })
+      .finally(() => fetchLogin());
+  }
+
+  return (
+    <Flex>
+      <Button onClick={() => navigate("/")}>home</Button>
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/write")}>write</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/signup")}>signup</Button>
+      )}
+      {isAdmin() && (
+        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+      )}
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/member?" + urlParams.toString())}>
+          회원정보
+        </Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/login")}>로그인</Button>
+      )}
+      {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
+    </Flex>
+  );
 }
