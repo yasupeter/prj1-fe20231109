1a818273dd09be5d66af01d98522562f6fdccbd3
yasupeter <peterleeful@gmail.com>
2023-11-15 15:39:21 +0900

    Wed, 11.15 useRef로 댓글 id 임시 저장

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -19,7 +19,7 @@ import {
   Textarea,
   useDisclosure,
 } from "@chakra-ui/react";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { DeleteIcon } from "@chakra-ui/icons";
 import { LoginContext } from "./LogInProvider";
@@ -83,10 +83,13 @@ function CommentList({ commentList, onDeleteModalOpen, isSubmitting }) {
 
 export function CommentContainer({ boardId }) {
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [id, setId] = useState(0);
   const [commentList, setCommentList] = useState([]);
 
   const { isOpen, onClose, onOpen } = useDisclosure();
+
+  // const [id, setId] = useState(0);
+  // useRef : 컴포넌트에서 임시로 값을 저장하는 용도로 사용
+  const commentIdRef = useRef(0);
 
   const { isAuthenticated } = useContext(LoginContext);
 
@@ -114,7 +117,7 @@ export function CommentContainer({ boardId }) {
     // TODO: 모달, then, catch, finally
 
     setIsSubmitting(true);
-    axios.delete("/api/comment/" + id).finally(() => {
+    axios.delete("/api/comment/" + commentIdRef.current).finally(() => {
       onClose();
       setIsSubmitting(false);
     });
@@ -122,7 +125,8 @@ export function CommentContainer({ boardId }) {
 
   function handleDeleteModalOpen(id) {
     // id 를 어딘가 저장
-    setId(id);
+    // setId(id);
+    commentIdRef.current = id;
     // 모달 열기
     onOpen();
   }
