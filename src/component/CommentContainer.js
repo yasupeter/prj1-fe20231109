35ffe0267b1d96bf294515cdabea083d7f01a060
yasupeter <peterleeful@gmail.com>
2023-11-15 12:18:41 +0900

    Wed, 11.15 댓글 리스트 key prop 추가

--- src/component/CommentContainer.js
+++ src/component/CommentContainer.js
@@ -40,7 +40,7 @@ function CommentList({ commentList }) {
       <CardBody>
         <Stack divider={<StackDivider />} spacing="4">
           {commentList.map((comment) => (
-            <Box>
+            <Box key={comment.id}>
               <Flex justifyContent="space-between">
                 <Heading size="xs">{comment.memberId}</Heading>
                 <Text fontSize="xs">{comment.inserted}</Text>
