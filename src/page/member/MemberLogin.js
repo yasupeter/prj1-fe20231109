3e9a51bbbe5d6746f5fde3d5c138609ed303284d
yasupeter <peterleeful@gmail.com>
2023-11-16 14:53:39 +0900

    Thurs, 11.16 경로 변경 시 서버 session의 속성(attribute) login 가져오도록 함

--- src/page/member/MemberLogin.js
+++ src/page/member/MemberLogin.js
@@ -1,10 +1,10 @@
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    useToast,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
 } from "@chakra-ui/react";
 import { useContext, useState } from "react";
 import axios from "axios";
@@ -12,53 +12,50 @@ import { useNavigate } from "react-router-dom";
 import { LoginContext } from "../../component/LogInProvider";
 
 export function MemberLogin() {
-    const [id, setId] = useState("");
-    const [password, setPassword] = useState("");
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
 
-    const navigate = useNavigate();
-    const toast = useToast();
+  const navigate = useNavigate();
+  const toast = useToast();
 
-    const { fetchLogin } = useContext(LoginContext);
+  const { fetchLogin } = useContext(LoginContext);
 
-    function handleLogin() {
-        axios
-            .post("/api/member/login", { id, password })
-            .then(() => {
-                toast({
-                    description: "로그인 되었습니다.",
-                    status: "info",
-                });
-                navigate("/");
-            })
-            .catch(() => {
-                toast({
-                    description: "아이디와 암호를 다시 확인해주세요.",
-                    status: "warning",
-                });
-            })
-            .finally(() => {
-                fetchLogin();
-            });
-    }
+  function handleLogin() {
+    axios
+      .post("/api/member/login", { id, password })
+      .then(() => {
+        toast({
+          description: "로그인 되었습니다.",
+          status: "info",
+        });
+        navigate("/");
+      })
+      .catch(() => {
+        toast({
+          description: "아이디와 암호를 다시 확인해주세요.",
+          status: "warning",
+        });
+      });
+  }
 
-    return (
-        <Box>
-            <h1>로그인</h1>
-            <FormControl>
-                <FormLabel>아이디</FormLabel>
-                <Input value={id} onChange={(e) => setId(e.target.value)} />
-            </FormControl>
-            <FormControl>
-                <FormLabel>암호</FormLabel>
-                <Input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-            </FormControl>
-            <Button colorScheme="blue" onClick={handleLogin}>
-                로그인
-            </Button>
-        </Box>
-    );
+  return (
+    <Box>
+      <h1>로그인</h1>
+      <FormControl>
+        <FormLabel>아이디</FormLabel>
+        <Input value={id} onChange={(e) => setId(e.target.value)} />
+      </FormControl>
+      <FormControl>
+        <FormLabel>암호</FormLabel>
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormControl>
+      <Button colorScheme="blue" onClick={handleLogin}>
+        로그인
+      </Button>
+    </Box>
+  );
 }
--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,126 +1,124 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import React, { useEffect, useState } from "react";
 import {
-    Box,
-    FormControl,
-    FormLabel,
-    Spinner,
-    Input,
-    Button,
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalCloseButton,
-    ModalBody,
-    ModalFooter,
-    useDisclosure,
-    useToast,
+  Box,
+  FormControl,
+  FormLabel,
+  Spinner,
+  Input,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
 export function MemberView() {
-    const [member, setMember] = useState(null);
-    // /member?id=userid
-    const [params] = useSearchParams();
+  const [member, setMember] = useState(null);
+  // /member?id=userid
+  const [params] = useSearchParams();
 
-    const { isOpen, onClose, onOpen } = useDisclosure();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
-    const navigate = useNavigate();
-    const toast = useToast();
+  const navigate = useNavigate();
+  const toast = useToast();
 
-    useEffect(() => {
-        axios
-            .get("/api/member?" + params.toString())
-            .then((response) => setMember(response.data))
-            .catch((error) => {
-                navigate("/login");
-                toast({
-                    description: "권한이 없습니다.",
-                    status: "warning",
-                });
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get("/api/member?" + params.toString())
+      .then((response) => setMember(response.data))
+      .catch((error) => {
+        navigate("/login");
+        toast({
+          description: "권한이 없습니다.",
+          status: "warning",
+        });
+      });
+  }, []);
 
-    if (member === null) {
-        return <Spinner />;
-    }
+  if (member === null) {
+    return <Spinner />;
+  }
 
-    function handleDelete() {
-        // axios
-        // delete /api/member?id=userid
-        // ok -> home 이동, toast 띄우기
-        // error -> toast 띄우기
-        // finally -> modal 닫기
+  function handleDelete() {
+    // axios
+    // delete /api/member?id=userid
+    // ok -> home 이동, toast 띄우기
+    // error -> toast 띄우기
+    // finally -> modal 닫기
 
-        axios
-            .delete("/api/member?" + params.toString())
-            .then(() => {
-                toast({
-                    description: "회원 탈퇴하였습니다.",
-                    status: "success",
-                });
-                navigate("/");
+    axios
+      .delete("/api/member?" + params.toString())
+      .then(() => {
+        toast({
+          description: "회원 탈퇴하였습니다.",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "탈퇴 처리 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onClose());
+  }
 
-                // TODO : 로그아웃 기능 추가하기
-            })
-            .catch((error) => {
-                if (error.response.status === 401 || error.response.status === 403) {
-                    toast({
-                        description: "권한이 없습니다.",
-                        status: "error",
-                    });
-                } else {
-                    toast({
-                        description: "탈퇴 처리 중에 문제가 발생하였습니다.",
-                        status: "error",
-                    });
-                }
-            })
-            .finally(() => onClose());
-    }
+  return (
+    <Box>
+      <h1>{member.id}님 정보</h1>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>별명</FormLabel>
+        <Input value={member.nickName} readOnly></Input>
+      </FormControl>
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input value={member.email} readOnly />
+      </FormControl>
+      <Button
+        colorScheme="purple"
+        onClick={() => navigate("/member/edit?" + params.toString())}
+      >
+        수정
+      </Button>
+      <Button colorScheme="red" onClick={onOpen}>
+        탈퇴
+      </Button>
 
-    return (
-        <Box>
-            <h1>{member.id}님 정보</h1>
-            <FormControl>
-                <FormLabel>password</FormLabel>
-                <Input type="text" value={member.password} readOnly />
-            </FormControl>
-            <FormControl>
-                <FormLabel>별명</FormLabel>
-                <Input value={member.nickName} readOnly></Input>
-            </FormControl>
-            <FormControl>
-                <FormLabel>email</FormLabel>
-                <Input value={member.email} readOnly />
-            </FormControl>
-            <Button
-                colorScheme="purple"
-                onClick={() => navigate("/member/edit?" + params.toString())}
-            >
-                수정
+      {/* 탈퇴 모달 */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>탈퇴 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>탈퇴 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme="red">
+              탈퇴
             </Button>
-            <Button colorScheme="red" onClick={onOpen}>
-                탈퇴
-            </Button>
-
-            {/* 탈퇴 모달 */}
-            <Modal isOpen={isOpen} onClose={onClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>탈퇴 확인</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>탈퇴 하시겠습니까?</ModalBody>
-
-                    <ModalFooter>
-                        <Button onClick={onClose}>닫기</Button>
-                        <Button onClick={handleDelete} colorScheme="red">
-                            탈퇴
-                        </Button>
-                    </ModalFooter>
-                </ModalContent>
-            </Modal>
-        </Box>
-    );
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
 }
