a6e2a4c422a24501e2deb2ac979462dca709ccfe
yasupeter <peterleeful@gmail.com>
2023-11-16 11:37:05 +0900

    Thurs, 11.16 게시물 리스트에 댓글 수 출력

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -1,63 +1,73 @@
 import React, { useEffect, useState } from "react";
 import {
-    Box,
-    Spinner,
-    Table,
-    Tbody,
-    Td,
-    Th,
-    Thead,
-    Tr,
+  Badge,
+  Box,
+  Spinner,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ChatIcon } from "@chakra-ui/icons";
 
 export function BoardList() {
-    const [boardList, setBoardList] = useState(null);
+  const [boardList, setBoardList] = useState(null);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        axios
-            .get("/api/board/list")
-            .then((response) => setBoardList(response.data));
-    }, []);
+  useEffect(() => {
+    axios
+      .get("/api/board/list")
+      .then((response) => setBoardList(response.data));
+  }, []);
 
-    if (boardList === null) {
-        return <Spinner />;
-    }
+  if (boardList === null) {
+    return <Spinner />;
+  }
 
-    return (
-        <Box>
-            <h1>게시물 목록</h1>
-            <Box>
-                <Table>
-                    <Thead>
-                        <Tr>
-                            <Th>id</Th>
-                            <Th>title</Th>
-                            <Th>by</Th>
-                            <Th>at</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {boardList.map((board) => (
-                            <Tr
-                                _hover={{
-                                    cursor: "pointer",
-                                }}
-                                key={board.id}
-                                onClick={() => navigate("/board/" + board.id)}
-                            >
-                                <Td>{board.id}</Td>
-                                <Td>{board.title}</Td>
-                                <Td>{board.nickName}</Td>
-                                <Td>{board.inserted}</Td>
-                            </Tr>
-                        ))}
-                    </Tbody>
-                </Table>
-            </Box>
-        </Box>
-    );
+  return (
+    <Box>
+      <h1>게시물 목록</h1>
+      <Box>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>id</Th>
+              <Th>title</Th>
+              <Th>by</Th>
+              <Th>at</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {boardList.map((board) => (
+              <Tr
+                _hover={{
+                  cursor: "pointer",
+                }}
+                key={board.id}
+                onClick={() => navigate("/board/" + board.id)}
+              >
+                <Td>{board.id}</Td>
+                <Td>
+                  {board.title}
+                  {board.countComment > 0 && (
+                    <Badge>
+                      <ChatIcon />
+                      {board.countComment}
+                    </Badge>
+                  )}
+                </Td>
+                <Td>{board.nickName}</Td>
+                <Td>{board.inserted}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
 }
