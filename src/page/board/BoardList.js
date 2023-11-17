16719280a0079d73a0a486ad4e1233548a198d0c
yasupeter <peterleeful@gmail.com>
2023-11-17 14:36:58 +0900

    Fri, 11.17 페이지 param (p=3) 적용

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -11,7 +11,7 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { ChatIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
@@ -19,11 +19,12 @@ import { faHeart } from "@fortawesome/free-solid-svg-icons";
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
 
+  const [params] = useSearchParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     axios
-      .get("/api/board/list")
+      .get("/api/board/list?" + params)
       .then((response) => setBoardList(response.data));
   }, []);
 
