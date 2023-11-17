bb163f68d8c66bafaca7c2860b44674c6bd53a75
yasupeter <peterleeful@gmail.com>
2023-11-17 15:42:58 +0900

    Fri, 11.17 페이지 버튼 만들기(임의)

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import {
   Badge,
   Box,
+  Button,
   Spinner,
   Table,
   Tbody,
@@ -26,7 +27,7 @@ export function BoardList() {
     axios
       .get("/api/board/list?" + params)
       .then((response) => setBoardList(response.data));
-  }, []);
+  }, [params]);
 
   if (boardList === null) {
     return <Spinner />;
@@ -74,6 +75,19 @@ export function BoardList() {
             ))}
           </Tbody>
         </Table>
+      </Box>
+
+      <Box>
+        <Button onClick={() => navigate("/?p=1")}>1</Button>
+        <Button onClick={() => navigate("/?p=2")}>2</Button>
+        <Button onClick={() => navigate("/?p=3")}>3</Button>
+        <Button onClick={() => navigate("/?p=4")}>4</Button>
+        <Button onClick={() => navigate("/?p=5")}>5</Button>
+        <Button onClick={() => navigate("/?p=6")}>6</Button>
+        <Button onClick={() => navigate("/?p=7")}>7</Button>
+        <Button onClick={() => navigate("/?p=8")}>8</Button>
+        <Button onClick={() => navigate("/?p=9")}>9</Button>
+        <Button onClick={() => navigate("/?p=10")}>10</Button>
       </Box>
     </Box>
   );
