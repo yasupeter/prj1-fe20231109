55793a24b35858595b0194046fbd810484088307
yasupeter <peterleeful@gmail.com>
2023-11-20 10:38:36 +0900

    Mon, 11.20 검색 후 페이지 이동 가능

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -22,6 +22,23 @@ import {
   faAngleRight,
   faHeart,
 } from "@fortawesome/free-solid-svg-icons";
+import * as PropTypes from "prop-types";
+
+function PageButton({ variant, pageNumber, children }) {
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    params.set("p", pageNumber);
+    navigate("/?" + params);
+  }
+
+  return (
+    <Button variant={variant} onClick={handleClick}>
+      {children}
+    </Button>
+  );
+}
 
 function Pagination({ pageInfo }) {
   const pageNumbers = [];
@@ -44,15 +61,15 @@ function Pagination({ pageInfo }) {
       )}
 
       {pageNumbers.map((pageNumber) => (
-        <Button
+        <PageButton
           key={pageNumber}
           variant={
             pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
           }
-          onClick={() => navigate("/?p=" + pageNumber)}
+          pageNumber={pageNumber}
         >
           {pageNumber}
-        </Button>
+        </PageButton>
       ))}
 
       {pageInfo.nextPageNumber && (
