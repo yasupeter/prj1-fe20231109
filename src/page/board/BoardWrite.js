79e198fa3793892f5235fcaa111d8dd104fac4b0
yasupeter <peterleeful@gmail.com>
2023-11-20 10:54:37 +0900

    Mon, 11.20 파일 전송 추가 중

--- src/page/board/BoardWrite.js
+++ src/page/board/BoardWrite.js
@@ -1,79 +1,89 @@
 import React, { useState } from "react";
 import {
-    Box,
-    Button,
-    FormControl,
-    FormLabel,
-    Input,
-    Textarea,
-    useToast,
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export function BoardWrite() {
-    const [title, setTitle] = useState("");
-    const [content, setContent] = useState("");
-    const [isSubmitting, setIsSubmitting] = useState(false);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [file, setFile] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const toast = useToast();
-    const navigate = useNavigate();
+  const toast = useToast();
+  const navigate = useNavigate();
 
-    function handleSubmit() {
-        setIsSubmitting(true);
-        axios
-            .post("/api/board/add", {
-                title,
-                content,
-            })
-            .then(() => {
-                toast({
-                    description: "새 글이 저장되었습니다.",
-                    status: "success",
-                });
-                navigate("/");
-            })
-            .catch((error) => {
-                console.log(error.response.status);
-                if (error.response.status === 400) {
-                    toast({
-                        description: "작성한 내용을 확인해주세요.",
-                        status: "error",
-                    });
-                } else {
-                    toast({
-                        description: "저장 중에 문제가 발생하였습니다.",
-                        status: "error",
-                    });
-                }
-            })
-            .finally(() => setIsSubmitting(false));
-    }
+  function handleSubmit() {
+    setIsSubmitting(true);
+    axios
+      .postForm("/api/board/add", {
+        title,
+        content,
+        file,
+      })
+      .then(() => {
+        toast({
+          description: "새 글이 저장되었습니다.",
+          status: "success",
+        });
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error.response.status);
+        if (error.response.status === 400) {
+          toast({
+            description: "작성한 내용을 확인해주세요.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "저장 중에 문제가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => setIsSubmitting(false));
+  }
 
-    return (
-        <Box>
-            <h1>게시물 작성</h1>
-            <Box>
-                <FormControl>
-                    <FormLabel>제목</FormLabel>
-                    <Input value={title} onChange={(e) => setTitle(e.target.value)} />
-                </FormControl>
-                <FormControl>
-                    <FormLabel>본문</FormLabel>
-                    <Textarea
-                        value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                    ></Textarea>
-                </FormControl>
+  return (
+    <Box>
+      <h1>게시물 작성</h1>
+      <Box>
+        <FormControl>
+          <FormLabel>제목</FormLabel>
+          <Input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </FormControl>
+        <FormControl>
+          <FormLabel>본문</FormLabel>
+          <Textarea
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          ></Textarea>
+        </FormControl>
+        <FormControl>
+          <FormLabel>이미지</FormLabel>
+          <Input
+            type="file"
+            accept="image/*"
+            onChange={(e) => setFile(e.target.files[0])}
+          />
+        </FormControl>
 
-                <Button
-                    isDisabled={isSubmitting}
-                    onClick={handleSubmit}
-                    colorScheme="blue"
-                >
-                    저장
-                </Button>
-            </Box>
-        </Box>
-    );
+        <Button
+          isDisabled={isSubmitting}
+          onClick={handleSubmit}
+          colorScheme="blue"
+        >
+          저장
+        </Button>
+      </Box>
+    </Box>
+  );
 }
